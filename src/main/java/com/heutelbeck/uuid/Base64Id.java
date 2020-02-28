2652a2b58564fdd69750bd76aec7a982270fc7f5
heutelbeck <dheutelbeck@ftk.de>
2020-02-28 10:05:50 +0100

    rename packages

--- src/main/java/com/heutelbeck/uuid/Base64Id.java
+++ src/main/java/com/heutelbeck/uuid/Base64Id.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.ftk.util;
+package com.heutelbeck.uuid;
 
 import java.util.UUID;
 
